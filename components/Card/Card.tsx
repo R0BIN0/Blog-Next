86e62fd420c352e99902800b08c5fbb6dea2aea3
Robin <rob.blanquart02100@gmail.com>
2022-05-07 22:28:39 +0200

    Card component UI added

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -3,7 +3,19 @@ import Link from "next/link";
 import styles from "./Card.module.css";
 
 const Card = () => {
-  return <></>;
+  return (
+    <div className={styles.card}>
+      <h2 className={styles.title}>Lisez L'article</h2>
+      <h3 className={styles.subtitle}>Maximiser votre culture web</h3>
+      <p className={styles.description}>
+        Chaque auteur tente de vous apporter le plus de valeur poisslbe par
+        article
+      </p>
+      <Link href={"/blog"}>
+        <a className={styles.link}>Visiter le blog</a>
+      </Link>
+    </div>
+  );
 };
 
 export default Card;
