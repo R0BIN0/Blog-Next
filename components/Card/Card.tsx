a1ee04f3d15249741cfdd8f455261af30db048c9
Robin <rob.blanquart02100@gmail.com>
2022-05-07 22:24:01 +0200

    Home page UI added

---
+++ components/Card/Card.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Link from "next/link";
+import styles from "./Card.module.css";
+
+const Card = () => {
+  return <></>;
+};
+
+export default Card;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,20 @@
 import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+import Card from "../components/Card/Card";
 
 const Home: NextPage = () => {
-  return <div>Home</div>;
+  return (
+    <section className={styles.section__container}>
+      <h1 className={styles.title}>Bienvenue Sur Code.io</h1>
+      <p className={styles.subtitle}>
+        Le blog communaitre des afficionados de développement web
+      </p>
+      <div className={styles.card__container}>
+        <Card />
+        <Card />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
