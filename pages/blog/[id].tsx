d680b80c6b444610adb7de95ff70093daec19788
Robin <rob.blanquart02100@gmail.com>
2022-05-08 22:18:04 +0200

    [ADD] Article page UI

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,5 +1,15 @@
+import styles from "../../styles/Article.module.css";
 const Article = () => {
-  return <div>Article</div>;
+  return (
+    <section className={styles.section__container}>
+      <h1 className={styles.title}>Qui est esse</h1>
+      <p className={styles.subtitle}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+        mollitia excepturi et temporibus maiores quos expedita beatae suscipit
+        vel ducimus!
+      </p>
+    </section>
+  );
 };
 
 export default Article;
