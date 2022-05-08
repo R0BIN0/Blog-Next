2f3b37b6d71ef73af4d6895662807f7fb335ef88
Robin <rob.blanquart02100@gmail.com>
2022-05-08 10:24:41 +0200

    [ADD] Blog Page UI

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,5 +1,33 @@
+import styles from "../../styles/Blog.module.css";
+import Card from "../../components/Card/Card";
+
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <section className={styles.section__container}>
+      <h1 className={styles.title}>Bienvenue sur le Blog.</h1>
+      <p className={styles.subtitle}>Voici les articles</p>
+      <div className={styles.card__container}>
+        <Card
+          title="fake title"
+          description="lorem ipsum dolor sit amet"
+          link="Lire cet article"
+          href="/blog/article1"
+        />
+        <Card
+          title="fake title"
+          description="lorem ipsum dolor sit amet"
+          link="Lire cet article"
+          href="/blog/article1"
+        />
+        <Card
+          title="fake title"
+          description="lorem ipsum dolor sit amet"
+          link="Lire cet article"
+          href="/blog/article1"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default Blog;
