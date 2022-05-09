0c399a4336e2e6d43a75f0e05cc29b10ed3a9b6c
Robin <rob.blanquart02100@gmail.com>
2022-05-09 11:30:49 +0200

    [ADD] User screen UI

--- pages/userList/[id].tsx
+++ pages/userList/[id].tsx
@@ -1,5 +1,32 @@
+import styles from "../../styles/User.module.css";
+
 const User = () => {
-  return <div>User</div>;
+  return (
+    <section>
+      <div className={styles.section__container}>
+        <div className={styles.user__card}>
+          <div className={styles.user__card__top}>
+            <p className={styles.user__name}>Clémentine Bauch</p>
+            <p className={styles.user__username}>Username : Samantha</p>
+          </div>
+          <div className={styles.user__card__bottom}>
+            <div className={styles.user__info__box}>
+              <p>Username : Samantha</p>
+            </div>
+            <div className={styles.user__info__box}>
+              <p>Email : samantha@laposte.net</p>
+            </div>
+            <div className={styles.user__info__box}>
+              <p>Site Web : samantha.com</p>
+            </div>
+            <div className={styles.user__info__box}>
+              <p>Téléphone : 1-463-123-4447</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default User;
