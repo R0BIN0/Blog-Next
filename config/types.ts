dd2201207c24a0e493f156981cf86e5fd6a96182
Robin <rob.blanquart02100@gmail.com>
2022-05-09 11:06:21 +0200

    [ADD] fetch data on userList screen

--- config/types.ts
+++ config/types.ts
@@ -1,3 +1,5 @@
+// ============================ POST ============================
+
 export interface getPost {
   data: Post;
 }
@@ -9,4 +11,40 @@ export interface Post {
   id: number;
   title: string;
   body: string;
+}
+
+// ============================ USER ============================
+
+export interface getUserResults {
+  data: User[];
+}
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+export interface Address {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: Geo;
+}
+
+export interface Geo {
+  lat: string;
+  lng: string;
+}
+
+export interface Company {
+  name: string;
+  catchPhrase: string;
+  bs: string;
 }
--- pages/userList/index.tsx
+++ pages/userList/index.tsx
@@ -1,26 +1,33 @@
+import { GetStaticProps } from "next";
 import Link from "next/link";
 import styles from "../../styles/UserList.module.css";
+import { getUserResults } from "../../config/types";
 
-const UserList = () => {
+const UserList = ({ data }: getUserResults) => {
   return (
     <section className={styles.section__container}>
       <h1 className={styles.title}>La liste des utilisateurs</h1>
       <div className={styles.users__container}>
-        <div className={styles.user__box}>
-          <p className={styles.user__title}>Bret</p>
-          <Link href={`/userList/1`}>
-            <a className={styles.user__link}>Contacter</a>
-          </Link>
-        </div>
-        <div className={styles.user__box}>
-          <p>Bret</p>
-          <Link href={`/userList/1`}>
-            <a className={styles.user__link}>Contacter</a>
-          </Link>
-        </div>
+        {data.map((item) => (
+          <div className={styles.user__box}>
+            <p className={styles.user__title}>{item.name}</p>
+            <Link href={`/userList/${item.id}`}>
+              <a className={styles.user__link}>Contacter</a>
+            </Link>
+          </div>
+        ))}
       </div>
     </section>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { data },
+  };
 };
 
 export default UserList;
