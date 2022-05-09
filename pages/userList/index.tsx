15c486dbce2b7aef8df41872791abe6900470493
Robin <rob.blanquart02100@gmail.com>
2022-05-09 10:18:14 +0200

    [ADD] userList screen UI

--- pages/userList/index.tsx
+++ pages/userList/index.tsx
@@ -1,5 +1,26 @@
+import Link from "next/link";
+import styles from "../../styles/UserList.module.css";
+
 const UserList = () => {
-  return <div>UserList</div>;
+  return (
+    <section className={styles.section__container}>
+      <h1 className={styles.title}>La liste des utilisateurs</h1>
+      <div className={styles.users__container}>
+        <div className={styles.user__box}>
+          <p className={styles.user__title}>Bret</p>
+          <Link href={`/userList/1`}>
+            <a className={styles.user__link}>Contacter</a>
+          </Link>
+        </div>
+        <div className={styles.user__box}>
+          <p>Bret</p>
+          <Link href={`/userList/1`}>
+            <a className={styles.user__link}>Contacter</a>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default UserList;
